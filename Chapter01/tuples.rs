b6bf80d92b5b060456d18024ce749c6ef3e61d07
Anatolij <java1cprog@yandex.ru>
2018-10-10 22:57:28 +0200

    unnecessarily import
    Using import `use std::{i8};` is unnecessarily 
    Mark first variable as unused - add `_`

--- Chapter01/tuples.rs
+++ Chapter01/tuples.rs
@@ -3,12 +3,12 @@
 // Version : 1.0
 // Date : 3 Dec 2016
 
-use std::{i8};
+
 
 fn main() {
 	
 	// Declaring a tuple
-	let rand_tuple = ("Mozilla Science Lab", 2016);
+	let _rand_tuple = ("Mozilla Science Lab", 2016);
 	let rand_tuple2 : (&str, i8) = ("Viki",4);
 
 	// tuple operations
