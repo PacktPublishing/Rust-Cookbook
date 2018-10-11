b59c9d068a53cbfdc325d1d2e54ffeec7e7d0896
Anatolij <java1cprog@yandex.ru>
2018-10-11 21:39:47 +0200

    Remove useless code
    Remove round brackets around `x` and debugging marker in `println` marcos

--- Chapter05/sample_move.rs
+++ Chapter05/sample_move.rs
@@ -9,10 +9,8 @@ use std::thread;
 
 fn main() {
     let x = 1;
-    
-    let handle = thread::spawn(move || {
-       	(x)
-    });
 
-    println!("{:?}", handle.join().unwrap());
+    let handle = thread::spawn(move || x);
+
+    println!("{}", handle.join().unwrap());
 }
