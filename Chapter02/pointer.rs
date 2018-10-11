0855d3936f2cce834633faab0542cb96b837c088
Anatolij <java1cprog@yandex.ru>
2018-10-11 09:27:47 +0200

    Rust style
    Add Rust style return from a function and fix unused warning.

--- Chapter02/pointer.rs
+++ Chapter02/pointer.rs
@@ -3,7 +3,7 @@
 // Version : 1.0
 // Date : 3 Dec 2016
 
-use std::{i32};
+
 
 fn main() {
 
@@ -15,7 +15,7 @@ fn main() {
 
 	let prim_val = 1;
 	let prim_val2 = prim_val;
-	println!("primitive value :- {}", prim_val);
+	println!("primitive value: {}", prim_val2);
 
 	// passing the ownership to the function
 	println!("Sum of vects : {}", sum_vects(&vect1));
@@ -27,5 +27,5 @@ fn main() {
 fn sum_vects (v1: &Vec<i32>) -> i32 {
 	// apply a closure and iterator
 	let sum = v1.iter().fold(0, |mut sum, &x | {sum += x; sum});
-	return sum;
+	sum
 }
